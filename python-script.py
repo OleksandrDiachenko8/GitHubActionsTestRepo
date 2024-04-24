90f325a6758881627c4c4db47f64757348609c97
Oleksandr Diachenko <alexdko.mail@gmail.com>
2024-04-24 21:03:55 +0300

    Create python-script.py

---
+++ python-script.py
@@ -0,0 +1,13 @@
+import csv
+
+with open("dataset-file") as file:    
+    tsv_file = csv.reader(file, delimiter="\t")    
+    linenumber = 0
+    for line in tsv_file:
+        linenumber += 1        
+        print(line)
+        with open("movies.txt", 'a') as f:
+            f.write(line + '\n')
+        if linenumber > 25:
+          break
+          
